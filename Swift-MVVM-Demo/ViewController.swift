83d30191b664546da2080bc045675560a1334c26
Parth Contractor <contractor110@gmail.com>
2019-11-17 10:10:30 +1100

    Initial Commit

---
+++ Swift-MVVM-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-MVVM-Demo
+//
+//  Created by Parth on 17/11/19.
+//  Copyright © 2019 Parth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
